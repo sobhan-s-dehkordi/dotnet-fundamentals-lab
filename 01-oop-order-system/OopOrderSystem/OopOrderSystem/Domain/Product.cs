b794280d60bf68884c8138d62c0ae6b6b5d4ff75
SobhanSoleyman <sobhan.soleyman@gmail.com>
2026-06-21 11:31:11 +0330

    Add ProductClass

---
+++ 01-oop-order-system/OopOrderSystem/OopOrderSystem/Domain/Product.cs
@@ -0,0 +1,33 @@
+﻿namespace OopOrderSystem.Domain;
+
+public abstract class Product
+{
+    public int Id { get; init; }
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+
+    protected Product(int id, string name, decimal price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        return $"[{Id}: {Name} - {Price}]";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Product other)
+            return Id == other.Id;
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
