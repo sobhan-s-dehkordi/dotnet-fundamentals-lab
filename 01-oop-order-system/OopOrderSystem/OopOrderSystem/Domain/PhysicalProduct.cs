66704bdd220021c77d094d4a38559ab9288ab877
SobhanSoleyman <sobhan.soleyman@gmail.com>
2026-06-21 11:34:39 +0330

    Add PhysicalProductClass

---
+++ 01-oop-order-system/OopOrderSystem/OopOrderSystem/Domain/PhysicalProduct.cs
@@ -0,0 +1,12 @@
+﻿namespace OopOrderSystem.Domain;
+
+public class PhysicalProduct : Product
+{
+    public double Weight { get; private set; }
+
+    public PhysicalProduct(int id, string name, decimal price, double weight) 
+        : base(id, name, price)
+    {
+        Weight = weight;
+    }
+}
