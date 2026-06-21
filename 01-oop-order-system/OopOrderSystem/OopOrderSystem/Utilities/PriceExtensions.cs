f9a85ed47e2486dfc70fa48115cc6ae8c460187d
SobhanSoleyman <sobhan.soleyman@gmail.com>
2026-06-21 12:17:00 +0330

    Add PriceExtensions

---
+++ 01-oop-order-system/OopOrderSystem/OopOrderSystem/Utilities/PriceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace OopOrderSystem.Utilities;
+
+public static class PriceExtensions
+{
+    public static decimal ApplyTax(this decimal price, decimal taxPercentage = 10)
+    {
+        return price + (price * (taxPercentage / 100));
+    }
+
+}
