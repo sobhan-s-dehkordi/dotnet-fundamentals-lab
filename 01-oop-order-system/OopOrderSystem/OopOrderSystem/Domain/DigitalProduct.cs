f4ceda1d18f5be554eb7c62ce7986b0716f438d3
SobhanSoleyman <sobhan.soleyman@gmail.com>
2026-06-21 11:37:56 +0330

    Add DigitalProductClass

---
+++ 01-oop-order-system/OopOrderSystem/OopOrderSystem/Domain/DigitalProduct.cs
@@ -0,0 +1,12 @@
+﻿namespace OopOrderSystem.Domain;
+
+public sealed class DigitalProduct : Product
+{
+    public string DownloadLink { get; private set; }
+
+    public DigitalProduct(int id, string name, decimal price, string downloadLink) 
+        : base(id, name, price) 
+    {
+        DownloadLink = downloadLink;
+    }
+}
